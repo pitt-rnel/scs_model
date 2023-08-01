3032822719920ddaa9fd0c0c5e417a2240155a30
xqfang <78752015+xqfang@users.noreply.github.com>
2023-08-01 12:52:22 -0400

    Update __init__.py
    removed muscle spindle

--- step_3/neuralnetwork/code/simulations/__init__.py
+++ step_3/neuralnetwork/code/simulations/__init__.py
@@ -1,6 +1,5 @@
 from .Simulation import Simulation
 from .CellsRecording import CellsRecording
 from .ForwardSimulation import ForwardSimulation
-from .ForSimMuscleSpindles import ForSimMuscleSpindles
 from .ForSimSpinalModulation import ForSimSpinalModulation
 from .CollisionEesNatural import CollisionEesNatural
